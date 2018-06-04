6816562caf5ec025e8c095178c2924b2cb7a309e
lee <hongyu.0802@gmail.com>
2018-06-04 18:07:53 +0200

    encapsulation

---
+++ workspace/reviser/src/bag1/Encap.java
@@ -0,0 +1,39 @@
+package bag1;
+
+class GoodDog{
+	private int size;
+	
+	public int getSize(){
+		return size;
+	}
+	public void setSize(int s) {
+		size=s;
+	}
+	
+	void bark(){
+		if(size>60){
+			System.out.println("Woof");
+		}
+		else if(size>14){
+			System.out.println("Ruff");
+		}
+		else{
+			System.out.println("Yip");
+		}
+	}
+}
+public class Encap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GoodDog one=new GoodDog();
+		GoodDog two=new GoodDog();
+		one.setSize(70);
+		two.setSize(8);
+		System.out.println("Dog one: "+one.getSize());
+		System.out.println("Dog two: "+two.getSize());
+		one.bark();
+		two.bark();
+	}
+
+}
